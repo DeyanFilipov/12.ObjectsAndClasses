4f90f69681c585ef7118d2e8ca191e8c34fe7802
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-02-22 23:27:58 +0200

    Commit

---
+++ Lab/Students_05.java
@@ -0,0 +1,62 @@
+package ObjectsAndClasses.Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Students_05 {
+    static class Student {
+        String firstName;
+        String lastName;
+        String age;
+        String town;
+
+        public Student (String firstName, String lastName, String age, String town) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.town = town;
+        }
+
+        public String getFirstName() {
+            return this.firstName;
+        }
+
+        public String getLastName() {
+            return this.lastName;
+        }
+
+        public String getAge() {
+            return this.age;
+        }
+
+        public String getTown() {
+            return this.town;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Student> studentList = new ArrayList<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("end")) {
+            String[] studentsDataArr = input.split(" ");
+            String firstName = studentsDataArr[0];
+            String lastName = studentsDataArr[1];
+            String age = studentsDataArr[2];
+            String town = studentsDataArr[3];
+
+            Student currentStudent = new Student(firstName, lastName, age, town);
+            studentList.add(currentStudent);
+
+            input = scanner.nextLine();
+        }
+        String homeTown = scanner.nextLine();
+        for (Student student : studentList) {
+            if (homeTown.equals(student.getTown())) {
+                System.out.printf("%s %s is %s years old\n", student.getFirstName(), student.getLastName(), student.getAge());
+            }
+        }
+    }
+}
